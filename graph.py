3bd6299431cae4e1686875c8c536e260f4241a5b
Ankit kandwal <sharmaankit49103@gmail.com>
2025-06-27 12:56:44 +0530

    Delete graph.py

--- graph.py
+++
@@ -1,9 +0,0 @@
-if __name__ == "__main__":
-   n, e = map(int, input().split())
-   adj = [[] for _ in range(n+1)]
-   for _ in range(e):
-       f, t = map(int, input().split())
-       adj[f].append(t)
-       adj[t].append(f)
-   for i in range(1, n+1):
-       print(f"{i} -> {' '.join(map(str, adj[i]))}")
